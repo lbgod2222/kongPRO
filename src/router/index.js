d306573f798d12b54c78b2ca335664d7135ec668
abise <lbgod2222@163.com>
2017-10-10 23:36:20 +0800

    to complete the router

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,38 @@ import Router from 'vue-router';
 
 Vue.use(Router);
 
-export default new Router({
+// 这里能定义方法
 
+export default new Router({
+  mode: 'history', // 试试非哈希
+  base: __dirname,
+  routes: [
+      { path: '/', component: Home }, // 主页
+      // 总话题域
+      { path: '/topic', component: Topic, meta: { current: 'all' },
+        // children: [
+        //     { path: 'all', component: Topic, meta: { current: 'all' } },
+        //     { path: 'progress', component: Topic, meta: { current: 'progress' } },
+        //     { path: 'public', component: Topic, meta: { current: 'public' } },
+        //     { path: 'done', component: Topic, meta: { current: 'done' } }
+        // ]
+      },
+      // 话题详情域
+      { path: '/topicExh', component: Topic_exh, 
+        children: [
+            { path: 'exhProgress', component: exhProgress },
+            { path: 'exhDetail', component: exhDetail },
+            { path: 'exhDone', component: exhDone }
+        ]
+      },
+      // 个人中心域
+      { path: '/personal', component: Personal,
+        children: [
+            { path: 'info', component: Personal_info },
+            { path: 'appeal', component: Personal_appeal },
+            { path: 'help', component: Personal_help },
+            { path: 'vote', component: Personal_vote }
+        ]
+      }
+  ]
 });
