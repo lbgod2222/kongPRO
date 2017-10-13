0176112c3709575dffbe08de78e808d1f0c51026
abise <lbgod2222@163.com>
2017-10-13 12:17:37 +0800

    done

---
+++ src/components/Modals/launchTopic/launchTopic.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="launchTopic-contain">
+    <p>launchTopic pop</p>
+  </div>
+</template>
+
+<script>
+export default { name: 'launchTopic' };
+</script>
+
+<style scoped>
+  .launchTopic-contain{
+      position: fixed;
+      top: 40%;
+      left: 40%;
+      background-color: rgba(37, 39, 40, .6);
+      width: 50px;
+      height: 50px;
+  }
+</style>
