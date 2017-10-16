5c1c3a2983247cf5338a28a6fc834fdce507a39a
dujunhui007 <810743626@qq.com>
2017-10-16 10:04:46 +0800

    personalInfo personalHelp

--- src/components/personal/personal-appeal/personal-appeal.vue
+++ src/components/personal/personal-appeal/personal-appeal.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="personal-right-contain">
-    <h1>PERSONAL APPEAL</h1>
+    <div class="personal-appeal">
+      <div class="personal-appeal-top">
+        <span class="appeal-image">图片</span>
+        <div class="appeal-top-right">
+          <p>世界杯中国VS日本</p>
+          <span>世界杯中国VS日本世界杯中国VS日本世界杯中国VS日本世界杯中国VS日本世界杯中国VS日本世界杯中国VS日本世界杯中国VS日本世界杯中国VS日本</span>
+          <p>参与金额:9873760</p>
+          <span>时间进度:60%</span>
+          <span>进度条进度条进度条进度条进度条进度条进度条进度条进度条进度条进度条进度条</span>
+        </div>
+      </div>
+      <div class="personal-appeal-center">
+        <span class="appeal-center-left">您已经对该话题进行申诉，仲裁委员将于24小时将会随机发送7名委员进行裁判</span>
+        <span class="appeal-center-right">等待裁决</span>
+      </div>
+      <div class="personal-appeal-bottom">
+        <span>饼图</span>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -17,4 +36,58 @@ export default { name: 'personal-appeal' };
       height: 600px;
       box-shadow: 0px 0px 10px rgb(26, 29, 29);
   }
+  .personal-appeal .personal-appeal-top .appeal-image{
+    display: inline-block;
+    height: 200px;
+    width: 30%;
+    background: red;
+    line-height: 200px;
+    text-align: center;
+  }
+  .personal-appeal .personal-appeal-top .appeal-top-right{
+    display: inline-block;
+    width: 65%;
+    height: 200px;
+    vertical-align:bottom;
+    font-size: 12px;
+    margin-left: 20px;
+  }
+  .personal-appeal .personal-appeal-top .appeal-top-right span{
+    display: block;
+    margin-top: 10px;
+  }
+  .personal-appeal .personal-appeal-top .appeal-top-right p{
+    font-size: 16px;
+    margin-top: 40px;
+  }
+  .personal-appeal .personal-appeal-center{
+    width: 100%;
+    margin: 10px 0;
+    background: #2a2c2d;
+    height: 50px;
+    line-height: 50px;
+  }
+  .personal-appeal .personal-appeal-center .appeal-center-left{
+    display: inline-block;
+    width: 80%;
+    height: 50px;
+    line-height: 50px;
+    margin-left: 20px;
+  }
+  .personal-appeal .personal-appeal-center .appeal-center-right{
+    display: inline-block;
+    float: right;
+    width: 18%;
+    height: 50px;
+    line-height: 50px;
+    text-align: center;
+    background: #22c9df;
+  }
+  .personal-appeal .personal-appeal-bottom{
+    height: 500px;
+    line-height: 500px;
+    text-align: center;
+    background: cadetblue;
+  }
+
 </style>
--- src/components/personal/personal-help/personal-help.vue
+++ src/components/personal/personal-help/personal-help.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="personal-right-contain">
-    <h1>PERSONAL HELP</h1>
+    <div class="personal-help">
+      <h4>帮组中心</h4>
+      <div class="personal-help-content">
+        <p>1.什么是声望，声望权限都有什么？</p>
+        <p>2.对于公布期答案，需要怎么样的仲裁？</p>
+        <p>3.关于转账手续费问题？</p>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -16,5 +24,21 @@ export default { name: 'personal-help' };
       width: 79.5%;
       height: 600px;
       box-shadow: 0px 0px 10px rgb(26, 29, 29);
+  }
+  .personal-help{
+    width: 100%;
+  }
+  .personal-help h4{
+    font-size: 14px;
+    height: 50px;
+    line-height: 50px;
+    margin-left: 20px;
+  }
+  .personal-help .personal-help-content p{
+    height: 40px;
+    line-height: 40px;
+    margin-left: 50px;
+    color: #ccc;
+    font-size: 12px;
   }
 </style>
